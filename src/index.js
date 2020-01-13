a1105a803f74046c27b454d2f61e263700d72578
MohamedMaQil <maqil625@gmail.com>
2020-01-13 02:36:29 +0100

    Fixed GitHub Pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ReactLeaflet from "./react-leaflet";
 
+import { HashRouter, Route, Switch } from "react-router-dom";
+
 import "./style.css";
+
+// class App extends React.Component {
+//   render() {
+//     return <ReactLeaflet path="react-leaflet" />;
+//   }
+// }
 
 class App extends React.Component {
   render() {
-    return <ReactLeaflet path="react-leaflet" />;
+    return (
+      <HashRouter basename="/">
+        <Switch>
+          <Route exact path="/" component={ReactLeaflet} />
+        </Switch>
+      </HashRouter>
+    );
   }
 }
 
