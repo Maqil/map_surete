98b78fb931c8d0f74a230825e8714c061952b474
MohamedMaQil <maqil625@gmail.com>
2020-01-13 02:37:04 +0100

    Updates

---
+++ precache-manifest.726f20cf8d4aab22638e61382fcb7aa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93305076c76c414c5f92",
+    "url": "/surete_map/static/css/main.873f89b5.chunk.css"
+  },
+  {
+    "revision": "93305076c76c414c5f92",
+    "url": "/surete_map/static/js/main.99b53fb7.chunk.js"
+  },
+  {
+    "revision": "afab8d6baaaa44dc68f7",
+    "url": "/surete_map/static/js/runtime~main.318d9039.js"
+  },
+  {
+    "revision": "0c51c011fdebbf24fb62",
+    "url": "/surete_map/static/js/2.51e8b1e5.chunk.js"
+  },
+  {
+    "revision": "12618163aec8be872f7698b961d3c873",
+    "url": "/surete_map/index.html"
+  }
+];
